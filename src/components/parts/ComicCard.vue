3877064a43cdf0eacdd3c3ab7798e5715494d45c
Gianluca Benassi <benox1199@gmail.com>
2021-12-14 14:40:54 +0100

    main comic cards

---
+++ src/components/parts/ComicCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <a href="#" class="card">
+        <img :src="comicImg" :alt="comicTitle">
+        <span>{{comicTitle}}</span>
+    </a>
+</template>
+
+<script>
+export default {
+    name: 'ComicCard',
+    props: {
+        comicImg: String,
+        comicTitle: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.card {
+    display: flex;
+    flex-direction: column;
+
+    img {
+        width: 100%;
+        max-height: 180px;
+        object-fit: cover;
+        object-position: top;
+    }
+    
+    span {
+        color: #fff;
+        font-size: 14px;
+        text-transform: uppercase;
+        margin-top: 20px;
+    }
+}
+
+</style>
